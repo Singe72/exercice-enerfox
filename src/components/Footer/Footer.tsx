a8f5d2a5c3beb9bb3cd2eda8f38424010eabb8c8
Thomas Hyaumet <t.hyaumet@free.fr>
2024-02-12 15:32:05 +0100

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+import { Layout } from "antd";
+
+export default function Footer() {
+    return (
+        <Layout.Footer className={styles.Footer}>
+            © Enerfox {new Date().getFullYear()}
+        </Layout.Footer>
+    );
+}
