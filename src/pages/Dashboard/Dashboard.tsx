d0f197307541931190668b3624ab3c0b5b487a26
Thomas Hyaumet <t.hyaumet@free.fr>
2024-02-13 12:04:36 +0100

    show a graph of daily power consumption

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,21 @@
 import styles from "./Dashboard.module.css";
 import { useEffect, useState } from "react";
-import type { DatePickerProps } from 'antd';
-import { DatePicker } from 'antd';
-import { IGetDeviceData, TIME_FRAME, getDeviceData } from "../../services/api";
+import { DatePicker } from "antd";
+import type { DatePickerProps } from "antd";
 import dayjs from "dayjs";
 import locale from "antd/es/date-picker/locale/fr_FR";
 import "dayjs/locale/fr";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { IGetDeviceData, TIME_FRAME, getDeviceData } from "../../services/api";
+
+type TransformedDeviceData = {
+    name: string;
+    puissance: number;
+}
 
 export default function Dashboard() {
-
     const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(dayjs());
+    const [deviceData, setDeviceData] = useState<TransformedDeviceData[]>();
 
     useEffect(() => {
         async function fetchData() {
@@ -20,23 +26,28 @@ export default function Dashboard() {
                 timeframe: TIME_FRAME.MINUTE,
             };
             const data = await getDeviceData(params);
-            console.log(data);
+            const transformedData = data.map(d => {
+                const { timestamp, values: { p } } = d;
+                return {
+                    name: new Date(timestamp).toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }),
+                    puissance: p,
+                };
+            });
+            setDeviceData(transformedData);
         }
         fetchData();
     }, [selectedDate]);
 
-    const handleDateChange: DatePickerProps["onChange"] = (date, dateString) => {
+    const handleDateChange: DatePickerProps["onChange"] = (date, _) => {
         if (!date) {
             return setSelectedDate(dayjs());
         }
         setSelectedDate(date);
     };
 
-
     return (
         <div className={styles.DashboardContainer}>
             <div className={styles.Chart}>
-                Graphique
                 <DatePicker
                     picker="date"
                     locale={locale}
@@ -44,12 +55,28 @@ export default function Dashboard() {
                     size="large"
                     placeholder="Sélectionnez une date"
                     defaultValue={selectedDate}
-                    onChange={handleDateChange}     
+                    onChange={handleDateChange}
                 />
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        width={500}
+                        height={100}
+                        data={deviceData}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" interval={119} />
+                        <YAxis unit=" W" />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="puissance" stroke="#82ca9d" dot={false} />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
-            <div className={styles.ChartLegend}>
-                <h2>Puissance consommée par votre appareil</h2>
-                <p>Données du <span>{selectedDate.locale("fr").format("dddd D MMMM YYYY")}</span></p>
+            <div className={styles.ChartLegendContainer}>
+                <div className={styles.ChartLegend}>
+                    <h2>Puissance consommée par votre appareil</h2>
+                    <p>Données du <span>{selectedDate.locale("fr").format("dddd D MMMM YYYY")}</span></p>
+                </div>
             </div>
         </div>
     );
