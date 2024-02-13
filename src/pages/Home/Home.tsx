0994ccff76319e20773ab019305ae71ef6f96859
Thomas Hyaumet <t.hyaumet@free.fr>
2024-02-13 14:32:46 +0100

    finalize home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
 import { Button } from "antd";
 
 export default function Home() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <Button type="primary">
-                Voir les données
-            </Button>
+        <div className={styles.HomeContainer}>
+            <div className={styles.Left}>
+                <div>
+                    <h1>Bonjour John !</h1>
+                    <p>Bienvenue sur votre espace Enerfox. Vous pouvez suivre votre consommation énergétique quotidienne en vous rendant sur votre tableau de bord.</p>
+                </div>
+                <Button type="primary" size="large" onClick={() => navigate("/dashboard")}>
+                    Accéder à mon tableau de bord
+                </Button>
+            </div>
+            <div className={styles.Right}>
+                <img src="/src/assets/enerfox_3d.webp" alt="Enerfox optimise vos performances énergétiques" />
+            </div>
         </div>
     );
 }
